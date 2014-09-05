7c84ec9fb3af3bd85ed716b3af24f2583402b80d
Jan Vornberger <jan@uos.de>
2014-09-05 21:22:44 +0200

    Initial commit

---
+++ src/bitpay_provider.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import re
+import requests
+
+API_URL = 'https://bitpay.com/api/'
+
+API_ENDPOINT_RATES = 'rates'
+CURRENCY_CODE_EUR = 'EUR'
+API_ENDPOINT_INVOICE = 'invoice'
+BITCOIN_URI_REGEX = '"bitcoin:[^"]*"'
+
+class BitPayProvider:
+    def __init__(self, api_key='', api_url=API_URL):
+        self.api_key = api_key
+        self.api_url = api_url
+
+    def get_euro_rate(self):
+        r = requests.get(self.api_url + API_ENDPOINT_RATES)
+        rates = r.json
+        for rate in rates:
+            if rate['code'] == CURRENCY_CODE_EUR:
+                return rate['rate']
+        raise Exception('Euro rate not provided.')
+
+    def create_invoice(self, price, currency, item_desc=None, marker=None):
+        payload = {'price': price, 'currency': currency}
+        if item_desc:
+            payload['itemDesc'] = item_desc
+        if marker:
+            payload['posData'] = marker
+        r = requests.post(self.api_url + API_ENDPOINT_INVOICE, data=payload,
+                auth=(self.api_key, ''))
+        if 'error' in r.json:
+            raise Exception(r.json['error']['message'])
+        invoice = BitPayInvoice.constructFromJSON(self.api_key, self.api_url, r.json)
+        return invoice
+
+class BitPayInvoice:
+    def __init__(self, invoice_id, url, status, bitcoin_uri):
+        self.invoice_id = invoice_id
+        self.url = url
+        self.status = status
+        self.bitcoin_uri = bitcoin_uri
+
+    def get_url(self):
+        return self.url
+
+    def get_bitcoin_uri(self):
+        return self.bitcoin_uri
+
+    @staticmethod
+    def constructFromJSON(api_key, api_url, json):
+        invoice_id = json['id']
+        url = json['url']
+        status = json['status']
+
+        r = requests.get(url)
+        match = re.search(BITCOIN_URI_REGEX, r.text)
+        if match:
+            bitcoin_uri = match.group(0)
+            bitcoin_uri = bitcoin_uri.replace('"', '').replace('&amp;', '&')
+            return BitPayInvoice(invoice_id, url, status, bitcoin_uri)
+        else:
+            raise Exception("Unable to extract Bitcoin URI")
---
+++ tests/test_bitpay-provider.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import requests
+import unittest
+
+from os.path import expanduser, isfile
+
+from bitpay_provider import BitPayProvider
+
+TEST_API_KEY_FILE = '.bitpay-brick-test-api-key'
+API_URL_WITH_IP = 'https://141.101.113.162/api/'
+
+def read_test_api_key():
+    api_key_file = expanduser('~') + '/' + TEST_API_KEY_FILE
+    if not isfile(api_key_file):
+        return None
+    with open(api_key_file, 'r') as f:
+        api_key = f.read()
+    return api_key.strip()
+
+TEST_API_KEY = read_test_api_key()
+
+class Test(unittest.TestCase):
+    def test_rejects_connection_without_verified_certificate(self):
+        bitpay_provider = BitPayProvider('', API_URL_WITH_IP)
+        with self.assertRaises(requests.models.SSLError):
+            bitpay_provider.get_euro_rate()
+
+    def test_current_euro_rate_is_nonzero(self):
+        bitpay_provider = BitPayProvider()
+        euro_rate = bitpay_provider.get_euro_rate()
+        self.assertTrue(euro_rate != 0)
+
+    def test_creating_an_invoice_without_a_key_fails(self):
+        bitpay_provider = BitPayProvider()
+        with self.assertRaises(Exception):
+            bitpay_provider.create_invoice(42, 'EUR')
+
+    @unittest.skipUnless(TEST_API_KEY != None, "no API key available")
+    def test_creating_a_simple_invoice_is_possible(self):
+        bitpay_provider = BitPayProvider(TEST_API_KEY)
+        invoice = bitpay_provider.create_invoice(42, 'EUR')
+        self.assertTrue(invoice.get_url().startswith('https://bitpay.com'),
+                "no BitPay URL provided for invoice")
+
+    @unittest.skipUnless(TEST_API_KEY != None, "no API key available")
+    def test_created_invoice_knows_its_bitcoin_uri(self):
+        bitpay_provider = BitPayProvider(TEST_API_KEY)
+        invoice = bitpay_provider.create_invoice(42, 'EUR')
+        self.assertTrue(invoice.get_bitcoin_uri().startswith('bitcoin:'))
+
+if __name__ == '__main__':
+    unittest.main()
