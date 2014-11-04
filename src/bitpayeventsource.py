870b6d3c37b1fd4f365cc472f91f55d0ebd07ecc
Jan Vornberger <jan@uos.de>
2014-11-04 01:26:01 +0100

    Extend event source support

--- src/bitpayeventsource.py
+++ src/bitpayeventsource.py
@@ -1,20 +1,63 @@
 #!/usr/bin/env python
 
-import urllib
+import json
 import requests
+import threading
 
-EVENT_URL = 'https://bitpay.com/events'
+from retrying import retry
+
+EVENT_ENDPOINT = 'https://bitpay.com/events'
 CONTENT_TYPE_HEADER = {'content-type': 'text/event-stream'}
 SUBSCRIBE_QUERY = 'action=subscribe&events%%5B%%5D=payment&token=%s'
+STATUS_FIELD = 'status'
+STATUS_EXPIRED = 'expired'
+STATUS_PAID = 'paid'
+STATUS_NO_CONNECTION = 'no connection'
 
-class BitPayEventSource:
-    def __init__(self, token):
+class BitPayEventSource(threading.Thread):
+    def __init__(self, token, callback, event_endpoint = EVENT_ENDPOINT,
+            retrying = True):
+        super(BitPayEventSource, self).__init__()
         self.token = token
+        self.callback = callback
+        self.event_endpoint = event_endpoint
+        self.retrying = retrying
 
-    def start(self):
+    def run(self):
+        if self.retrying:
+            self.connect_with_retrying()
+        else:
+            try:
+                self.connect()
+            except requests.ConnectionError:
+                self.callback(STATUS_NO_CONNECTION)
+
+    @retry(wait_exponential_multiplier=250, wait_exponential_max=30000) # ms
+    def connect_with_retrying(self):
+        self.connect()
+
+    def connect(self):
         params = SUBSCRIBE_QUERY % self.token
-        r = requests.get(EVENT_URL, headers=CONTENT_TYPE_HEADER, params=params,
-                stream=True)
-        print r.url
+        r = requests.get(self.event_endpoint, headers=CONTENT_TYPE_HEADER,
+                params=params, stream=True)
         for line in r.iter_lines(chunk_size=16):
-            print line
+            if ':' not in line:
+                continue
+
+            (field, value) = line.strip().split(':', 1)
+            field = field.strip()
+
+            if field != "data":
+                continue
+
+            try:
+                j = json.loads(value)
+                if not STATUS_FIELD in j:
+                    continue
+
+                if j[STATUS_FIELD] == STATUS_EXPIRED:
+                    self.callback(STATUS_EXPIRED)
+                if j[STATUS_FIELD] == STATUS_PAID:
+                    self.callback(STATUS_PAID)
+            except ValueError:
+                pass
--- tests/test_bitpayeventsource.py
+++ tests/test_bitpayeventsource.py
@@ -1,13 +1,86 @@
 #!/usr/bin/env python
 
+import BaseHTTPServer
+import json
+import threading
+import time
 import unittest
 
-from bitpayeventsource import BitPayEventSource
+from Queue import Queue
+
+from bitpayeventsource import BitPayEventSource, STATUS_EXPIRED, STATUS_PAID, \
+        STATUS_NO_CONNECTION
+
+TEST_PORT = 8481
+TEST_ENDPOINT = 'http://127.0.0.1:8481/'
+TEST_TOKEN = 'testtoken'
+CONTENT_EXPIRED = {'status': 'expired'}
+CONTENT_PAID = {'status': 'paid'}
+
+class OneShotServer(threading.Thread):
+    def __init__(self, port, json_content):
+        super(OneShotServer, self).__init__()
+        self.port = port
+        self.json_content = json_content
+
+    def run(self):
+        server = BaseHTTPServer.HTTPServer(('127.0.0.1', self.port),
+                OneShotHandler)
+        server.json_content = self.json_content
+        server.handle_request()
+
+class OneShotHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.wfile.write('\ndata:%s\n' %
+                json.dumps(self.server.json_content))
+
+    def log_request(self, code):
+        pass    # no logging
 
 class Test(unittest.TestCase):
-    def test_one(self):
-        event_source = BitPayEventSource('7ij57s6enEmja36x2G3efJ8k5zVxEBPjgRWsAa8jbg8B1stptUzvBYvQAfoa6q2eYq')
+    def test_callback_fires_for_expired_invoice(self):
+        one_shot_server = OneShotServer(TEST_PORT, CONTENT_EXPIRED)
+        one_shot_server.start()
+        q = Queue()
+        event_source = BitPayEventSource(TEST_TOKEN,
+                callback = lambda status:q.put(status),
+                event_endpoint = TEST_ENDPOINT)
         event_source.start()
+        status = q.get()
+        self.assertEqual(status, STATUS_EXPIRED)
+
+    def test_returns_no_connection_status_on_error_when_retrying_is_disabled(self):
+        q = Queue()
+        event_source = BitPayEventSource(TEST_TOKEN,
+                callback = lambda status:q.put(status),
+                event_endpoint = TEST_ENDPOINT, retrying = False)
+        event_source.start()
+        status = q.get()
+        self.assertEqual(status, STATUS_NO_CONNECTION)
+
+    def test_retries_on_connection_errors(self):
+        q = Queue()
+        event_source = BitPayEventSource(TEST_TOKEN,
+                callback = lambda status:q.put(status),
+                event_endpoint = TEST_ENDPOINT, retrying = True)
+        event_source.start()
+        time.sleep(0.1)
+        one_shot_server = OneShotServer(TEST_PORT, CONTENT_EXPIRED)
+        one_shot_server.start()
+        status = q.get()
+        self.assertEqual(status, STATUS_EXPIRED)
+
+    def test_callback_for_paid_invoice_fires(self):
+        one_shot_server = OneShotServer(TEST_PORT, CONTENT_PAID)
+        one_shot_server.start()
+        q = Queue()
+        event_source = BitPayEventSource(TEST_TOKEN,
+                callback = lambda status:q.put(status),
+                event_endpoint = TEST_ENDPOINT)
+        event_source.start()
+        status = q.get()
+        self.assertEqual(status, STATUS_PAID)
 
 if __name__ == '__main__':
     unittest.main()
