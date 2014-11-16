fd6deb27773cbf8d4ee97a7c69702a7b9d6084c1
Jan Vornberger <jan@uos.de>
2014-11-16 14:23:12 +0100

    Improve basic checkout demo

--- src/bitpay-brick.py
+++ src/bitpay-brick.py
@@ -17,6 +17,9 @@ api_key = read_api_key()
 if not api_key:
     raise Exception("Unable to load API key.")
 
+frontend = Frontend(frontend_type = Frontend.TYPE_FRONTEND_SMALL_DISPLAY)
+frontend.start()
+
 bitpay_provider = BitPayProvider(api_key)
 invoice = bitpay_provider.create_invoice(DEFAULT_AMOUNT, DEFAULT_CURRENCY)
 
@@ -33,18 +36,17 @@ nfc_broadcast.start()
 nfc_broadcast.set_btc_uri(
         invoice.get_bitcoin_uri_with_bluetooth_address(bluetooth_address))
 
-frontend = Frontend(frontend_type = Frontend.TYPE_FRONTEND_SMALL_DISPLAY)
-frontend.start()
-frontend.show_invoice(invoice.get_bitcoin_uri())
-
 def check_status(status):
     if (status == STATUS_PAID):
         frontend.show_paid()
 invoice.watch(check_status)
 
+frontend.show_invoice(invoice.get_bitcoin_uri())
 print invoice.get_url()
 raw_input("Press return to exit")
 
+frontend.shutdown()
 invoice.stop_watching()
 nfc_broadcast.shutdown()
-# TODO: stop Bluetooth services
+payment_request_service.stop()
+tx_submission_service.stop()
