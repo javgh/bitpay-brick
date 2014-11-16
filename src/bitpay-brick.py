ad1342f3960dcb65d0d6f65497c63410308c40be
Jan Vornberger <jan@uos.de>
2014-11-16 03:29:49 +0100

    Implement a basic version of the checkout process

--- src/bitpay-brick.py
+++ src/bitpay-brick.py
@@ -2,32 +2,48 @@
 
 import readline
 
+from bitpayeventsource import STATUS_PAID
 from bitpayprovider import BitPayProvider
+from bluetoothservice import BluetoothPaymentRequestService, \
+        BluetoothTxSubmissionService
 from config import read_api_key
 from frontend import Frontend
-from invoicer import Invoicer
 from nfcbroadcast import NFCBroadcast
 
 DEFAULT_CURRENCY = 'EUR'
+DEFAULT_AMOUNT = 0.01
 
 api_key = read_api_key()
 if not api_key:
     raise Exception("Unable to load API key.")
 
 bitpay_provider = BitPayProvider(api_key)
-frontend = Frontend()
-frontend.start()
+invoice = bitpay_provider.create_invoice(DEFAULT_AMOUNT, DEFAULT_CURRENCY)
+
+payment_request_service = \
+        BluetoothPaymentRequestService(invoice.get_payment_request())
+payment_request_service.start()
+tx_submission_service = BluetoothTxSubmissionService(invoice.get_bip70_url())
+tx_submission_service.start()
+
+bluetooth_address = tx_submission_service.get_bluetooth_address()
+
 nfc_broadcast = NFCBroadcast()
 nfc_broadcast.start()
-invoicer = Invoicer(bitpay_provider, frontend, nfc_broadcast)
+nfc_broadcast.set_btc_uri(
+        invoice.get_bitcoin_uri_with_bluetooth_address(bluetooth_address))
 
-try:
-    while True:
-        price = raw_input('Price for new invoice in %s or Ctrl-D to exit: ' % DEFAULT_CURRENCY)
-        price_float = float(price)
-        invoicer.new_invoice(price, DEFAULT_CURRENCY)
-except EOFError:
-    pass
+frontend = Frontend(frontend_type = Frontend.TYPE_FRONTEND_SMALL_DISPLAY)
+frontend.start()
 
-frontend.shutdown()
+def check_status(status):
+    if (status == STATUS_PAID):
+        frontend.show_paid()
+invoice.watch(check_status)
+
+print invoice.get_url()
+raw_input("Press return to exit")
+
+invoice.stop_watching()
 nfc_broadcast.shutdown()
+# TODO: stop Bluetooth services
